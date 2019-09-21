e83050e12d8f32a569d63bcdf00aa7f98ceff989
JustPro10 <almonev@abv.bg>
2019-09-22 00:24:15 +0100

    Use images

--- src/Click.java
+++ src/Click.java
@@ -1,5 +1,11 @@
-import javax.swing.*;
-import java.awt.*;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Image;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.Random;
@@ -8,30 +14,45 @@ public class Click {
 
     private static final Random random = new Random();
 
+    private static Color[] COLORS = {Color.BLUE, Color.CYAN, Color.ORANGE, Color.RED, Color.YELLOW, Color.GREEN,
+            Color.PINK, Color.MAGENTA, Color.GRAY, Color.WHITE};
+
     private JFrame frame = new JFrame("Click");
     private static int width;
     private static int height;
+    private static int imageWidth = 320;
+    private static int imageHeight = 200;
     private static int objects = 0;
 
     private Click() {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
         frame.setLayout(null);
+        frame.setUndecorated(true);
         frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
         frame.setVisible(true);
-//        frame.setUndecorated(true);
-//        frame.pack();
         width = frame.getWidth();
         height = frame.getHeight();
-        frame.getContentPane().setBackground(Color.getHSBColor(0.6f, 0.9f, 0.95f));
 
-        initialise();
+        ImageIcon image = new ImageIcon("images/tram.jpg");
+        Image image2 = image.getImage();
+        Image newimg = image2.getScaledInstance(imageWidth, imageHeight,  Image.SCALE_SMOOTH);
+        image = new ImageIcon(newimg);
+
+        System.out.println("tram coming!");
+        JLabel label = new JLabel();
+        label.setIcon(image);
+        label.setSize(imageWidth, imageHeight);
+        frame.getContentPane().add(label);
+
+//        initialise();
     }
 
     private void initialise() {
         System.out.println("NEW");
 
         System.out.println(frame.getComponents().length);
+        frame.getContentPane().setBackground(COLORS[random.nextInt(10)]);
         final int value = random.nextInt(10);
 
         for (int i = 0; i < 3; i++) {
@@ -54,40 +75,32 @@ public class Click {
         label.addMouseListener(new MouseListener() {
             @Override
             public void mouseClicked(final MouseEvent e) {
-                if (!label.getText().equals("")) {
-                    label.setText("");
-                    objects--;
-                }
+                frame.remove(label);
+                frame.revalidate();
+                frame.repaint();
 
-                if (objects == 0) initialise();
-//                frame.remove(label);
-//                frame.revalidate();
+                if (--objects == 0) initialise();
             }
 
             @Override
             public void mousePressed(final MouseEvent e) {
-
             }
 
             @Override
             public void mouseReleased(final MouseEvent e) {
-
             }
 
             @Override
             public void mouseEntered(final MouseEvent e) {
-
             }
 
             @Override
             public void mouseExited(final MouseEvent e) {
-
             }
         });
 
         frame.add(label);
-        objects++;
-        System.out.println(objects);
+        System.out.println(++objects);
     }
 
     public static void main(String[] args) {
